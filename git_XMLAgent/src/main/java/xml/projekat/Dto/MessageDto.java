d62a8fe33a9b74e227a7406214b94f9a5a112ffe
Milica <Milica@DESKTOP-BMHOU8M>
2018-09-04 05:17:17 +0200

    DTO-porukice

---
+++ git_XMLAgent/src/main/java/xml/projekat/Dto/MessageDto.java
@@ -0,0 +1,38 @@
+package xml.projekat.Dto;
+
+
+
+public class MessageDto {
+
+	private long senderId;
+	
+	private long receiverId;
+	
+	private String content;
+
+	public long getSenderId() {
+		return senderId;
+	}
+
+	public void setSenderId(long senderId) {
+		this.senderId = senderId;
+	}
+
+	public long getReceiverId() {
+		return receiverId;
+	}
+
+	public void setReceiverId(long receiverId) {
+		this.receiverId = receiverId;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+}
